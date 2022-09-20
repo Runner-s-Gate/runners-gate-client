af4c7be6cdc1df68b3783b07395469c06101957e
Alexander Hjelm <alexander.hjelm@solidify.dev>
2022-09-20 18:00:01 +0200

    Add CD scripts

---
+++ cd-build.ps1
@@ -0,0 +1,3 @@
+& 'C:\Program Files\Godot\Godot_v3.5-stable_mono_win64.exe' --path C:\actions-runner\_work\runners-gate-client\runners-gate-client --export "Windows Desktop"
+Start-Sleep -Seconds 40.0
+cp C:\actions-runner\_work\runners-gate-client\runners-gate-client\config-prod.cfg C:\actions-runner\_work\runners-gate-client\runners-gate-client\release\config.cfg
